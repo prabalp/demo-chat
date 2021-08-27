950666945f7d7f14a9eb0bed58ceb7f0b866d8ba
prabalp <prabalpanda2008@gmail.com>
2021-08-27 13:11:36 +0530

    register1

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, KeyboardAvoidingView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button, Input, Image } from "react-native-elements";
 
-const LoginScreen = () => {
+const LoginScreen = (navigation) => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
 
@@ -39,7 +39,12 @@ const LoginScreen = () => {
       </View>
 
       <Button containerStyle={styles.button} onPress={signIn} title="Login" />
-      <Button containerStyle={styles.button} type="outline" title="Regiser" />
+      <Button
+        containerStyle={styles.button}
+        onPress={() => navigation.navigate("Register")}
+        type="outline"
+        title="Regiser"
+      />
       <View style={{ height: 100 }} />
     </KeyboardAvoidingView>
   );
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,64 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements/dist/buttons/Button";
+import { Input } from "react-native-elements/dist/input/Input";
+
+const RegisterScreen = (navigation) => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [imageUrl, setimageUrl] = useState("");
+
+  const register = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text>Create Account</Text>
+      <View>
+        <Input
+          placeholder="Full Name"
+          autoFocus
+          type="Text"
+          value={name}
+          onChangeText={(text) => setname(text)}
+        />
+        <Input
+          placeholder="Eamil"
+          type="Email"
+          value={email}
+          onChangeText={(text) => setemail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setpassword(text)}
+        />
+        <Input
+          placeholder="Profile Pic URL"
+          type="Text"
+          value={imageUrl}
+          onChangeText={(text) => setimageUrl(text)}
+          onSubmitEditing={register}
+        />
+
+        <Button
+          style={styles.button}
+          raised
+          onPress={register}
+          title="Register"
+        />
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+  button: {},
+});
